aa3db056dc0b8771f99e15a56f5e1df9316af71e
chithanhvp94 <chithanhvp94@gmail.com>
2019-09-23 23:00:15 -0700

    backup 09-23-2019

--- backup.sh
+++ backup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 NOW=$(date +"%m-%d-%Y")
-mysqldump -uladystarsv28c0 -pa1a864621ded1fd8 ladystars_v_28c0 > ladystars_db.sql
-git checkout -b backup/backup$NOW
-git add .
-git commit -a -m 'backup '$NOW
-git push origin backup/backup$NOW
+mysqldump -uladystarsv28c0 -pa1a864621ded1fd8 ladystars_v_28c0 > ladystars_db.sql >> /dev/null 2>&1
+git checkout -b backup/backup$NOW >> /dev/null 2>&1
+git add . >> /dev/null 2>&1
+git commit -a -m 'backup '$NOW >> /dev/null 2>&1
+git push origin backup/backup$NOW >> /dev/null 2>&1
+echo backup done
 
